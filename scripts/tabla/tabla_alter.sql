19f1a2b98d1b40bb2ca642917f254a403925a1c2
Cristian tucno conde <ctc.tucno@gmail.com>
2022-11-18 10:52:47 -0500

    comandos alter table

---
+++ scripts/tabla/tabla_alter.sql
@@ -0,0 +1,55 @@
+----------------------------
+-- tabla producto
+----------------------------
+create table dbo.product(
+id serial primary key,
+denominacion varchar(20) not null,
+cant integer not null,
+precio double precision not null
+);
+
+----------------------------
+-- insertar datos a la tabla
+----------------------------
+insert into dbo.product(denominacion,cant,precio)
+values('agua mineral angel',10,1.5);
+
+----------------------------
+-- modificar nombre tabla
+----------------------------
+alter table dbo.product rename to producto;
+
+----------------------------
+-- adicionar columna
+----------------------------
+alter table dbo.producto
+add column estado varchar(1) default '1';
+
+----------------------------
+-- aumentar tamaño columna
+----------------------------
+alter table dbo.producto
+alter column denominacion type varchar(60);
+
+----------------------------
+-- modificar tipo de dato
+----------------------------
+alter table dbo.producto
+alter column denominacion set data type text;
+
+----------------------------
+-- cambiar nombre columna
+----------------------------
+alter table dbo.producto
+rename column cant to cantidad;
+
+----------------------------
+-- eliminar columna
+----------------------------
+alter table dbo.producto
+drop column estado;
+
+----------------------------
+-- eliminar tabla
+----------------------------
+drop table dbo.producto;
