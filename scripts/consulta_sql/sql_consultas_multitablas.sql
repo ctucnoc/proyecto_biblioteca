1b9af768fc8524c64fafb22cc6628e4c2e6f70ff
ctucnoc <ctc.tucno@gmail.com>
2023-02-12 19:27:42 -0500

    actualizando inner join

--- scripts/consulta_sql/sql_consultas_multitablas.sql
+++ scripts/consulta_sql/sql_consultas_multitablas.sql
@@ -9,15 +9,27 @@
 
 ---------------------------------------------- ****EJEMPLOS PRACTICOS****---------------------------------------
 -- 1) OBTENER EL NOMBRE DE LAS EDITORIALES Y LOS LIBROS ORDENADOS POR EL NOMBRE DE LA EDITORIAL.
-select ed.nombre as editorial,li.titulo from dbo.adm_libro li
+select 
+ed.nombre as editorial,
+li.titulo 
+from dbo.adm_libro li
 inner join dbo.adm_editorial ed
-on li.id_editorial = ed.id_editorial order by 1 asc;
+on li.id_editorial = ed.id_editorial 
+order by 1 asc;
 
-select * from dbo.adm_libro li, dbo.adm_editorial ed
-where li.id_editorial = ed.id_editorial order by 1 asc;
+select 
+ed.nombre as editorial,
+li.titulo  
+from dbo.adm_libro li, dbo.adm_editorial ed
+where li.id_editorial = ed.id_editorial 
+order by 1 asc;
 
 -- 2) OBTENER EL NOMBRE DE LAS AREAS, SUB AREAS DE LOS LIBROS ORDENADOS POR EL TITULO DEL LIBRO.
-select ar.descripcion as area,sa.descripcion as sub_area, li.titulo from dbo.adm_area ar
+select 
+ar.descripcion as area,
+sa.descripcion as sub_area, 
+li.titulo 
+from dbo.adm_area ar
 inner join dbo.adm_sub_area sa
 on ar.id_area = sa.id_area
 inner join dbo.adm_libro li
@@ -25,7 +37,10 @@ on sa.id_sub_area = li.id_sub_area
 order by 3 asc;
 
 -- 3) OBTENER LOS LIBROS CON SUS RESPECTIVOS AUTORES ORDENADOS POR EL TITULO DEL LIBRO.
-select au.nombre || au.apellidos as autor, li.titulo from dbo.adm_libro li
+select 
+au.nombre || ' ' || au.apellidos as autor, 
+li.titulo 
+from dbo.adm_libro li
 inner join dbo.adm_libro_autor la
 on li.id_libro = la.id_libro
 inner join dbo.adm_autor au
