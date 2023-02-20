837b21260f7a283813bce14cf40ef1fac494f805
ctucnoc <ctc.tucno@gmail.com>
2023-02-20 12:07:25 -0500

    funcion 001

--- scripts/functions/functions.sql
+++ scripts/functions/functions.sql
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------------------------------------------------
 --- FUNCIONES DEFINIDAS POR EL USUARIO
 ---------------------------------------------------------------------------------------------------------------
--- :: LaSON BLOQUES DE CODIGO ES DECIR ESTRUCTURAS QUE AGLUTINAN UNA SERIE DE INSTRUCCIONES CON EL FIN DE EVITAR VOLVER A REPETIR EL CODIGO
+-- :: SON BLOQUES DE CODIGO ES DECIR ESTRUCTURAS QUE AGLUTINAN UNA SERIE DE INSTRUCCIONES CON EL FIN DE EVITAR VOLVER A REPETIR EL CODIGO
 
 -- =========================================== TIPOS DE LENGUAJE EN POSGRESQL ============================================
 ----- 1) SQL
@@ -17,8 +17,67 @@ $$
 DECLARE
  variable;
 BEGIN
+END;
 $$
 LANGUAGE 'tipo_lenguaje_utilizado'
 
 ---------------------------------------------- ****EJEMPLOS PRACTICOS****---------------------------------------
+create or replace function dbo.fn_saludo()
+returns varchar
+as
+$$
+begin
+ return 'hola Mundo';
+end;
+$$
+language 'plpgsql';
 
+create or replace function dbo.fn_sumar(
+in nro_a integer,
+in nro_b integer
+)
+returns integer
+as
+$$
+declare
+ v_suma integer := 0;
+begin
+ v_suma:= nro_a + nro_b;
+ return v_suma;
+end;
+$$
+language 'plpgsql';
+
+create or replace function dbo.suma_n(
+in nro_n integer
+)
+returns integer
+as
+$$
+declare
+ v_suma integer:= 0;
+begin
+ v_suma:= nro_n*(nro_n + 1)/2;
+ return v_suma;
+end;
+$$
+language 'plpgsql';
+
+create or replace function dbo.suma_while(
+in nro integer
+)
+returns integer
+as
+$$
+declare
+ v_suma integer:= 0;
+ v_cont integer:= 1;
+begin
+ while v_cont <= nro loop
+  v_suma:= v_suma + v_cont;
+  v_cont:= v_cont +1;
+ end loop;
+ return v_suma;
+end;
+$$
+language 'plpgsql';
